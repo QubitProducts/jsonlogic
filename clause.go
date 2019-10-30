871e872fc20dc36baa1c9d3365bfec552034a4f7
Tristan Colgate <tcolgate@gmail.com>
2019-10-30 20:16:19 +0000

    tests: Import tests from the jsonlogic docs.
    This implements tests from the jsonlogic introduction. I'll find more
    complete tests as/when I can, and once these pass.

--- clause.go
+++ clause.go
@@ -1,5 +1,10 @@
 package jsonlogic
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 // Operator repsents a jsonlogic Operator.
 type Operator struct {
 	Name string
@@ -13,32 +18,57 @@ type Argument struct {
 	float64
 }
 
+type Arguments []Argument
+
+func (args *Arguments) UnmarshalJSON(bs []byte) error {
+	panic("not implemented")
+}
+
 // Clause representes a jsonlogic clause.
 type Clause struct {
 	Operator  Operator
-	Arguments []Argument
+	Arguments Arguments
 }
 
 // UnmarshalJSON parses JSON data as a jsonlogic
 // Clause.
 func (c *Clause) UnmarshalJSON(bs []byte) error {
-	return nil
+	args := map[string]Arguments{}
+	maperr := json.Unmarshal(bs, &args)
+	if maperr == nil {
+		panic("map not implemented")
+	}
+
+	var truthy bool
+	trutherr := json.Unmarshal(bs, &truthy)
+	if trutherr == nil {
+		panic("bool not implemented")
+	}
+
+	switch {
+	case maperr != nil:
+		return fmt.Errorf("jsonlogic parse: %w", maperr)
+	case trutherr != nil:
+		return fmt.Errorf("jsonlogic parse: %w", trutherr)
+	default:
+		return fmt.Errorf("jsonlogic parse: could not parse as valid clause")
+	}
 }
 
 // MarshalJSON marshals a jsonlogic Clause into
 // pure JSON.
 func (c *Clause) MarshalJSON() ([]byte, error) {
-	return nil, nil
+	panic("not implemented")
 }
 
 // EvalJSON evluates this clause against the supplied
 // data. The data is parsed as json, and raw json
 // is returned.
 func (c *Clause) EvalJSON(data []byte) ([]byte, error) {
-	return nil, nil
+	panic("not implemented")
 }
 
 // Eval evaluates this clause against the provided data.
 func (c *Clause) Eval(data interface{}) (interface{}, error) {
-	return nil, nil
+	panic("not implemented")
 }
--- clause_test.go
+++ clause_test.go
@@ -1,6 +1,11 @@
 package jsonlogic
 
-import "testing"
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestClauseEval(t *testing.T) {
 	type test struct {
@@ -8,10 +13,70 @@ func TestClauseEval(t *testing.T) {
 		rule   string
 		data   interface{}
 		expect interface{}
-		expErr string
+		expErr assert.ErrorAssertionFunc
 	}
 
 	tests := []test{
+		{
+			name:   "invalid",
+			rule:   `x`,
+			expect: true,
+			expErr: assert.Error,
+		},
+		{
+			name:   "simple",
+			rule:   `{ "==" : [1, 1] }`,
+			expect: true,
+			expErr: assert.NoError,
+		},
+		{
+			name: "compound",
+			rule: `{"and" : [
+							 { ">" : [3,1] },
+							 { "<" : [1,3] }
+						 ] }`,
+			expect: true,
+		},
+		{
+			name:   "data-driven",
+			rule:   `{ "var" : ["a"] }`,
+			data:   map[string]float64{"a": 1},
+			expect: 1,
+		},
+		{
+			name:   "data-driven-sugar",
+			rule:   `{ "var" : "a" }`,
+			data:   map[string]float64{"a": 1},
+			expect: 1,
+		},
+		{
+			name:   "data-driven-array",
+			rule:   `{ "var" : 1 }`,
+			data:   []string{"apple", "banana", "carrot"},
+			expect: "banana",
+		},
+		{
+			name: "data-driven-mixed",
+			rule: `{ "and" : [
+								{"<" : [ { "var" : "temp" }, 110 ]},
+								{"==" : [ { "var" : "pie.filling" }, "apple" ] }
+							] }`,
+			data: map[string]interface{}{
+				"temp": 100.0,
+				"pie":  map[string]string{"filling": "apple"},
+			},
+			expect: true,
+		},
+		{
+			name:   "always",
+			rule:   `true`,
+			expect: true,
+		},
+		{
+			name:   "never",
+			rule:   `false`,
+			expect: false,
+		},
 		{
 			name: "fizzbuzz",
 			rule: `{ "if": [
@@ -26,11 +91,26 @@ func TestClauseEval(t *testing.T) {
 
 							{ "var": "i" }
 						 ]}`,
+			data: 20,
 		},
 	}
 
 	for _, st := range tests {
 		t.Run(st.name, func(t *testing.T) {
+			var c Clause
+			err := json.Unmarshal([]byte(st.rule), &c)
+			assert.EqualError(t, err, st.expErr)
+			if err != nil {
+				return
+			}
+
+			v, err := c.Eval(st.data)
+			assert.EqualError(t, err, st.expErr)
+			if err != nil {
+				return
+			}
+
+			assert.Equal(t, v, st.expect)
 		})
 	}
 }
