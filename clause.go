4a9e9219b35cb20b2c7bbb61435f523fdfaaa756
Tristan Colgate <tcolgate@gmail.com>
2021-08-02 09:22:58 +0100

    perf: check if array might contain clauses before trying to treat them as naked array clauses

--- clause.go
+++ clause.go
@@ -76,6 +76,26 @@ func (args *Arguments) UnmarshalJSON(bs []byte) error {
 	return fmt.Errorf("could not parse arguments")
 }
 
+// sliceHasPossibleClause recursivelychecks slices to see if they contain a dictionary
+// that could be a clause.  This lets us treat slices as clauses only in the cases
+// where we have to
+func sliceHasPossibleClause(ifs []interface{}) bool {
+	for i := range ifs {
+		switch v := ifs[i].(type) {
+		case []interface{}:
+			if sliceHasPossibleClause(v) {
+				return true
+			}
+		case map[string]interface{}:
+			if len(v) == 1 {
+				return true
+			}
+		default:
+		}
+	}
+	return false
+}
+
 // Clause represents a JsonLogic clause.
 type Clause struct {
 	Operator  Operator
@@ -99,24 +119,30 @@ func (c *Clause) UnmarshalJSON(bs []byte) error {
 		}
 	}
 
-	argsArr := []Argument{}
-	err = json.Unmarshal(bs, &argsArr)
-	if err == nil && len(argsArr) != 0 {
-		*c = Clause{
-			Arguments: argsArr,
-		}
-		return nil
-	}
-
 	var raw interface{}
 	err = json.Unmarshal(bs, &raw)
 	if err != nil {
 		return err
 	}
-	// this is a bit subtle, we want to differentiate instances of the empty
-	// slice, this forces a new slice header.
-	if rawslice, ok := raw.([]interface{}); ok && len(rawslice) == 0 {
-		raw = make([]interface{}, 0, 1)
+	if rawslice, ok := raw.([]interface{}); ok {
+		// this is a bit subtle, we want to differentiate instances of the empty
+		// slice, this forces a new slice header.
+		if len(rawslice) == 0 {
+			raw = make([]interface{}, 0, 1)
+		}
+
+		if sliceHasPossibleClause(rawslice) {
+			// We'll check again to see if this could be a slice being
+			// used as a naked array clause which might require evaluation.
+			argsArr := []Argument{}
+			err = json.Unmarshal(bs, &argsArr)
+			if err == nil && len(argsArr) != 0 {
+				*c = Clause{
+					Arguments: argsArr,
+				}
+				return nil
+			}
+		}
 	}
 	*c = Clause{
 		Arguments: []Argument{{
